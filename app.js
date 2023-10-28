b23b4bfc249eae41bddfcfbce259061c2c2185f6
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-28 21:59:36 +0200

    result

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+  return text.split('').reverse().join('');
+};
+
+const seznam = document.querySelectorAll('li');
+
+seznam.forEach((li) => {
+  if (
+    li.textContent.toLowerCase() === textPozpatku(li.textContent.toLowerCase())
+  ) {
+    li.classList.add('palindrom');
+  }
+});
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
